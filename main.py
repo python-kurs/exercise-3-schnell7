dabff2e690723293e7f6e8429431c8847f833a2e
ro <mail2benr@gmail.com>
2019-04-27 12:25:51 +0200

    Add achievable points to tasks

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 # Exercise 3
 
 
-# 1. Contstruct the path to the text file in the data directory using the `pathlib` module
+# 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
 
 
-# 2. Read the text file
+# 2. Read the text file [2P]
 
 
-# 3. Count the occurences of each item in the text file
+# 3. Count the occurences of each item in the text file [2P]
 
 
-# 4. Using `pathlib` check if a directory with name `solution` exists and if not create it
+# 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
 
 
-# 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header):
+# 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header): [2P]
 #    item, count
 #    item_name_1, item_count_1
 #    item_name_2, item_count_2
