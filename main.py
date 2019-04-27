548e80fcd94cf4a98c84d94b0580fda6ecf3cf94
ro <mail2benr@gmail.com>
2019-04-27 12:22:18 +0200

    Add main and utils file with instructions

---
+++ main.py
@@ -0,0 +1,20 @@
+# Exercise 3
+
+
+# 1. Contstruct the path to the text file in the data directory using the `pathlib` module
+
+
+# 2. Read the text file
+
+
+# 3. Count the occurences of each item in the text file
+
+
+# 4. Using `pathlib` check if a directory with name `solution` exists and if not create it
+
+
+# 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header):
+#    item, count
+#    item_name_1, item_count_1
+#    item_name_2, item_count_2
+#    ...
