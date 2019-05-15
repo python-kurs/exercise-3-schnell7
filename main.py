29fa0ca89b8b2d2ca46f34699b28f04c7147f79e
schnell7-1 <schnell7@students.uni-marburg.de>
2019-05-15 15:33:45 +0200

    first

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 # Exercise 3
 from pathlib import Path
+import utils
 # import functions from utils here
 
-data_dir = Path("data")
+data_dir = Path("H:\git\exercise-3-schnell7\data")
 output_dir = Path("solution")
-
+#%%
 # 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
-
-
+#data_dir = Path("H:\git\exercise-3-schnell7\data")
+data_dir = "H:\git\exercise-3-schnell7\data"
+#????????????
+#%%
 # 2. Read the text file [2P]
-
-
+with open(data_dir +"\cars.txt", "r") as file:
+    lines = file.readlines()
+    
+print(lines)
+#%%
 # 3. Count the occurences of each item in the text file [2P]
-
-
+count = utils.count_type()
+#%%
 # 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
 
 
@@ -22,3 +28,13 @@ output_dir = Path("solution")
 #    item_name_1, item_count_1
 #    item_name_2, item_count_2
 #    ...
+    
+single = list(set(lines))
+#%%
+
+with open(data_dir + "\solotions.csv", "w") as file:
+    file.write('Type;count\n' )
+    if len(res) == len(single):
+        for i in range(len(res)):
+            #file.write(str(res[i]) + ";" + single[i])
+            file.writelines(single[i][:-2] + ";" + str(res[i]) +"\n")
--- utils.py
+++ utils.py
@@ -2,3 +2,14 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+#%%
+def count_type(lines):
+    res = []
+    for i in set(lines):
+        c = 0
+        for k in lines:
+            if i == k:
+                c=c+1
+        res.append(c)
+    return(res)
+#%%
