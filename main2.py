e54f5e0937942dfa1bb918cb05d03b5e5db696fc
schnell7 <schnell7@students.uni-marburg.de>
2019-05-20 18:54:30 +0200

    sec

---
+++ main2.py
@@ -0,0 +1,14 @@
+import utils
+
+with open("./data/cars.txt", "r") as file:
+    lines = file.readlines()
+
+count = utils.count_type(lines)
+single = list(set(lines))
+
+with open("./solutions/count2.csv", "w") as file:
+    file.write('Type;count\n' )
+    if len(count) == len(single):
+        for i in range(len(count)):
+            #file.write(str(res[i]) + ";" + single[i])
+            file.writelines(single[i][:-2] + ";" + str(count[i]) +"\n")
